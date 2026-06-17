a51a521f36adb00027873dac90103e23131114e9
Baymaxxxu <411212105@mahasiswa.undira.ac.id>
2026-06-17 19:23:41 +0700

    Sesuaikan role user menggunakan is_admin

--- ctx.tsx
+++ ctx.tsx
@@ -42,4 +42,56 @@ export function SessionProvider({ children }: PropsWithChildren) {
         {children}
         </AuthContext.Provider>
     );
+}export type PoskoUser = {
+  id?: number;
+  username?: string | null;
+  password?: string | null;
+  is_admin?: boolean | null;
+  created_at?: string | null;
+  role?: string | null;
+  [key: string]: any;
+};mport { createContext, use, type PropsWithChildren } from 'react';
+
+import { useStorageState } from './useStorageState';
+
+const AuthContext = createContext<{
+  signIn: () => void;
+  signOut: () => void;
+  session?: string | null;
+  isLoading: boolean;
+}>({
+  signIn: () => null,
+  signOut: () => null,
+  session: null,
+  isLoading: false,
+});
+
+// Use this hook to access the user info.
+export function useSession() {
+  const value = use(AuthContext);
+  if (!value) {
+    throw new Error('useSession must be wrapped in a <SessionProvider />');
+  }
+
+  return value;
+}
+
+export function SessionProvider({ children }: PropsWithChildren) {
+    const [[isLoading, session], setSession] = useStorageState('session');
+
+    return (
+        <AuthContext.Provider
+            value={{
+                signIn: () => {
+                    setSession('xxx');
+                },
+                signOut: () => {
+                    setSession(null);
+                },
+                session,
+                isLoading,
+            }}>
+        {children}
+        </AuthContext.Provider>
+    );
 }
